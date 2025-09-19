c83361e03a9f1fda3ec190f9d7f7a48d1d835aaa
Yvonnick FRIN <frin.yvonnick@gmail.com>
2025-09-19 20:19:51 +0200

    Export types

--- godot_multiplayer.gd
+++ godot_multiplayer.gd
@@ -1,12 +1,36 @@
 @tool
 extends EditorPlugin
 
+const ROOT := "res://addons/godot-multiplayer"
+const TYPES: Array[Dictionary] = [
+	{
+		"name": "LobbyClient",
+		"base": "Node",
+		"script": ROOT + "/lobby/lobby_client.gd",
+		"icon": ROOT + "/icon.svg"
+	},
+	{
+		"name": "LobbyServer",
+		"base": "Node",
+		"script": ROOT + "/lobby/lobby_client.gd",
+		"icon": ROOT + "/icon.svg"
+	},
+	{
+		"name": "GameInstance",
+		"base": "Node",
+		"script": ROOT + "/game_instance/game_instance.gd",
+		"icon": ROOT + "/icon.svg"
+	}
+]
+
 
 func _enter_tree():
 	# Initialization of the plugin goes here.
-	pass
+	for type in TYPES:
+		add_custom_type(type.name, type.base, load(type.script), load(type.icon))
 
 
 func _exit_tree():
 	# Clean-up of the plugin goes here.
-	pass
+	for type in TYPES:
+		remove_custom_type(type.name)
